2c42b70450aff7458f740baf8bab2ce58378104d
alexla10 <garnicaalex100@gmail.com>
2024-12-12 12:04:34 -0300

    despliegue 4

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
  export const TOKEN_SECRET = process.env.TOKEN_SECRET || 'some secret key'
 
- export const DB_URL = process.env.DATABASE_URL || 'mongodb://localhost:27017/merndb';
+ export const DB_URL = process.env.DB_URL || 'mongodb://localhost:27017/merndb';
 
  export const FRONT_END_URL = process.env.FRONT_END_URL || 'http://localhost:5173';
 
