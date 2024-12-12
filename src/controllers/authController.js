9dd51f9e558778d5cdaaae7271a6bd0af3eae816
alexla10 <garnicaalex100@gmail.com>
2024-12-12 15:02:29 -0300

    despliegue 12

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -19,7 +19,7 @@ export const register = async (req,res) => {
         
         const userSaved = await newUser.save()
         const token = await createAccessToken({id: userSaved._id})
-        res.cookie("token",token, { httpOnly: true, secure: true,sameSite:'None',maxAge:24*60*60*1000})
+        res.cookie("token",token, { httpOnly: true, secure: process.env.NODE_ENV === 'production',sameSite:'None',maxAge:24*60*60*1000})
 
         res.json({
             id:userSaved._id,
@@ -48,7 +48,7 @@ export const login = async (req,res) => {
        
         
         const token = await createAccessToken({id: userFound._id})
-        res.cookie("token",token,{ httpOnly: true, secure: true,sameSite:'None',maxAge:24*60*60*1000})
+        res.cookie("token",token,{ httpOnly: true, secure: process.env.NODE_ENV === 'production',sameSite:'None',maxAge:24*60*60*1000})
 
         res.json({
             id:userFound._id,
