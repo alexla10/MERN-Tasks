ad7297b9192e07fc1d04cf6d141be35c6b7c9dfd
alexla10 <garnicaalex100@gmail.com>
2024-12-12 15:09:16 -0300

    despliegue 15

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,6 +1,8 @@
+import taskImage from '../img/tasks.jpg'
+
 function HomePage() {
   return (
-    <div className="justify-center items-center flex bg-[url('src/img/tasks.jpg')] min-h-screen bg-cover bg-center">
+    <div className="justify-center items-center flex min-h-screen bg-cover bg-center" style={{ backgroundImage: `url(${taskImage})` }}>
       <div className="max-w-4xl w-full px-6 py-12 bg-zinc-600 rounded-lg shadow-md mt-4">
         <h1 className="text-2xl md:text-4xl font-bold text-center text-white mb-6">
           Organiza y gestiona tus tareas de manera eficiente.
