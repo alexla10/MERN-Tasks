8379a784016419a6df62e5fbbcdae6aef8a084a2
alexla10 <garnicaalex100@gmail.com>
2024-12-13 19:23:04 -0300

    despliegue ulti 2.0

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -63,7 +63,7 @@ export const login = async (req,res) => {
 }
 
 export const logout = (req,res) =>{
-    res.cookie("token","",{expires: new Date(0)})
+    res.cookie("token","",{expires: new Date(0),httpOnly:true, secure:true, sameSite:'None'})
     return res.status(200).send("Logout successful"); 
 }
 
